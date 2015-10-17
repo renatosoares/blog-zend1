1b78dfe1fac41edc366747179a3b1026870b47f1
Renato Soares <renatosoarespro@hotmail.com>
2015-10-17 12:08:59 -0300

    configurações zend efetuadas

---
+++ data/tmp/templates_c/%%0E^0E4^0E407559%%footer.tpl.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.18, created on 2015-10-17 17:03:34
+         compiled from footer.tpl */ ?>
+        </div>
+    </body>
+</html>
---
+++ data/tmp/templates_c/%%83^833^83385F9A%%index.tpl.php
@@ -0,0 +1,15 @@
+<?php /* Smarty version 2.6.18, created on 2015-10-17 17:03:34
+         compiled from index/index.tpl */ ?>
+<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => 'header.tpl', 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
+
+Web site home
+
+<?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => 'footer.tpl', 'smarty_include_vars' => array()));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?>
